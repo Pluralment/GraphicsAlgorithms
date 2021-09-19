30437e019285b60b5911034e286a44e6c4af414b
Kamar <kmprojects1998@gmail.com>
2021-09-19 16:35:53 +0300

    Add WPF project.

---
+++ ACG/ACG/GraphicsModeler/Matrixes/Matrixes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace GraphicsModeler.Matrixes
+{
+    internal static class Matrixes
+    {
+        public static Matrix4x4 GetZAxisRotateMatrix(float degree)
+        {
+            return new Matrix4x4(
+                (float)Math.Cos(degree), (float)Math.Sin(degree), 0, 0,
+                -(float)Math.Sin(degree), (float)Math.Cos(degree), 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1);
+        }
+
+        public static Matrix4x4 GetXAxisRotateMatrix(float degree)
+        {
+            return new  Matrix4x4(1, 0, 0, 0,
+                                  0, (float)Math.Cos(degree), -(float)Math.Sin(degree), 0 ,
+                                  0, (float)Math.Sin(degree), (float)Math.Cos(degree), 0 ,
+                                  0, 0, 0, 1);
+        }
+        public static Matrix4x4 GetYAxisRotateMatrix(float degree)
+        {
+            return new Matrix4x4((float)Math.Cos(degree), 0, (float)Math.Sin(degree), 0,
+                                 0, 1, 0, 0,
+                                 -(float)Math.Sin(degree), 0, (float)Math.Cos(degree), 0,
+                                 0, 0, 0, 1);
+        }
+        public static Matrix4x4 GetTranslationMatrix(float deltaX, float deltaY, float deltaZ)
+        {
+            return new Matrix4x4(1, 0, 0, deltaX,
+                                 0, 1, 0, deltaY,
+                                 0, 0, 1, deltaZ,
+                                 0, 0, 0, 1);
+        }
+
+        public static Matrix4x4 GetScaleMatrix(float scaleCofficient)
+        {
+            return new Matrix4x4(scaleCofficient, 0, 0, 0,
+                                 0, scaleCofficient, 0, 0,
+                                 0, 0, scaleCofficient, 0,
+                                 0, 0, 0, 1);
+        }
+    }
+}
---
+++ ACG/ACG/GraphicsModeler/Parser/ObjectFileParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Numerics;
+using GraphicsModeler.Shapes;
+
+namespace GraphicsModeler.Parser
+{
+    internal class ObjectFileParser
+    {
+        private string _content;
+        private string[] _lines;
+        private Vector4[] _vectors;
+        public ObjectFileParser(string fileName)
+        {
+            if (fileName is null)
+                throw new ArgumentNullException(nameof(fileName));
+
+            _content = File.ReadAllText(fileName);
+            _lines = _content.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim()).ToArray();
+            _vectors = _lines.Where(x => x.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0].Equals("v", StringComparison.OrdinalIgnoreCase))
+                                .Select(x => new Vector4(float.Parse(x.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1]),
+                                                          float.Parse(x.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[2]),
+                                                          float.Parse(x.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[3]),
+                                                          1))
+                                .ToArray();
+        }
+
+
+        public List<Polygon> GetPolygons()
+        {
+            List<Polygon> polygonList = new List<Polygon>();
+            var faces = _lines.Where(x => x.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0].Equals("f", StringComparison.OrdinalIgnoreCase)).ToArray();
+            foreach (string face in faces)
+            {
+                var coords = face.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Skip(1).ToArray();
+                int i0 = int.Parse(coords[0].Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries)[0]) - 1;
+                int i1 = int.Parse(coords[1].Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries)[0]) - 1;
+                int i2 = int.Parse(coords[2].Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries)[0]) - 1;
+                polygonList.Add(new Polygon(new List<Vector4>()
+                {
+                    new Vector4(_vectors[i0].X, _vectors[i0].Y, _vectors[i0].Z, 1),
+                    new Vector4(_vectors[i1].X, _vectors[i1].Y, _vectors[i1].Z, 1),
+                    new Vector4(_vectors[i2].X, _vectors[i2].Y, _vectors[i2].Z, 1)
+                }));
+            }
+
+            return polygonList;
+        }
+
+    }
+}
